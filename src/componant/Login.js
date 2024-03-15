093a5a2c5dc1fd3e7d04d4b7695daa36fda7e0d3
NMB662001 <nishabhujbal6@gmail.com>
2024-03-15 16:50:40 +0800

    login form

--- src/componant/Login.js
+++ src/componant/Login.js
@@ -7,28 +7,29 @@ const Login = () => {
     return (
         <div className='body'>
             <div className='main'>
-                <form>
+                <form >
                     <h2 className='title'>LOGIN </h2>
                     <p className='line1'>Please Enter Your Login And Password !</p>
 
                     <div className='up'>
                         <label className='name'>UserName: </label>
-                        <input className='input' placeholder='Enter Your UserName' />
+                        <input className='input' placeholder='Enter Your UserName' required />
 
                     </div>
                     <br></br>
                     <div className='up'>
                         <label className='name'>PassWord : </label>
-                        <input className='input' placeholder='Enter Your PassWord' />
+                        <input className='input' placeholder='Enter Your PassWord' required />
 
                     </div>
                     <div className='line2'>
                         <a href="#!">Forgot password?</a>
                     </div>
                     <div >
-                        <button className='btn' onClick={() => {
+                        {/* <button className='btn' onClick={() => {
                             alert("Login Successfully!")
-                        }}>LOGIN</button>
+                        }}>LOGIN</button> */}
+                        <button className="btn" type='submit'>LOGIN</button>
                     </div>
                     <div className='icons'>
                         {/* <a href='/'><FaFacebook /></a> */}
